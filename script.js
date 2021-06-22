730cd17a1eed1d59ae8e6f8c3e03f20ab97191e0
Ricardo <rr155914@gmail.com>
2021-06-21 20:30:41 -0500

    Check linters

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById('myNav').style.height = "100%";
 }
+openNav;
 
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+  document.getElementById('myNav').style.height = "0%";
 }
+closeNav;
