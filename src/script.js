bbcd9fe38d036b35ec48418d2e23540a4fd94f9e
Ricardo <rr155914@gmail.com>
2021-06-23 14:25:08 -0500

    Add js object

---
+++ src/script.js
@@ -0,0 +1,72 @@
+// Responsive menu
+function openNav() {
+  document.getElementById('myNav').style.height = '100%';
+}
+openNav();
+
+function closeNav() {
+  document.getElementById('myNav').style.height = '0%';
+}
+closeNav();
+
+//Pop up
+
+const projects = [
+  {
+    name: 'Topic',
+    feature: 'CANOPY',
+    background: 'Back End Dev',
+    year: '2015',
+    image: '/img/SnapshootPortfoliox.png',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    tag1: 'html',
+    tag2: 'css',
+    tag3: 'javascript',
+    linkLive: 'https://richie9000.github.io/Portfolio-setup/',
+    linkSource: 'https://richie9000.github.io/Portfolio-setup/',
+  },
+  {
+    name: 'Multi-Post Stories',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: '/img/SnapshootPortfolio4.png',
+    description:
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    tag1: 'html',
+    tag2: 'css',
+    tag3: 'javascript',
+    linkLive: 'https://richie9000.github.io/Portfolio-setup/',
+    linkSource: 'https://richie9000.github.io/Portfolio-setup/',
+  },
+  {
+    name: 'Facebook 360',
+    feature: 'FACEBOOK',
+    background: 'Full Stack Dev',
+    year: '2015',
+    image: 'img/facebook-360-project.svg',
+    description:
+      "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    tag1: 'html',
+    tag2: 'css',
+    tag3: 'javascript',
+    linkLive: 'https://richie9000.github.io/Portfolio-setup/',
+    linkSource: 'https://richie9000.github.io/Portfolio-setup/',
+  },
+  {
+    name: 'Uber Navigation',
+    feature: 'Uber',
+    background: 'Lead Developer',
+    year: '2018',
+    image: 'img/facebook-360-project.svg',
+    description:
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    tag1: 'html',
+    tag2: 'css',
+    tag3: 'javascript',
+    linkLive: 'https://richie9000.github.io/Portfolio-setup/',
+    linkSource: 'https://richie9000.github.io/Portfolio-setup/',
+  },
+];
+
